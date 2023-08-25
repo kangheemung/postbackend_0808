3f788b4866188893c325f3cdeda66e98efa3e598
EC2 Default User <ec2-user@ip-10-0-0-80.ap-northeast-1.compute.internal>
2023-08-25 15:46:57 +0000

    add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,35 @@
 class UsersController < ApplicationController
   include Authenticatable
-  after_action :csrf_token, only: [:show,:update]
+
+  after_action :csrf_token, only: [:show, :update]
   skip_before_action :verify_authenticity_token, only: [:signup]
 
   def new
     @user = User.new
-    render json: { status: 'ture', message: 'User signed up successfully' }
+    render json: { status: 'true', message: 'User signed up successfully' }
   end
 
   def signup
     user = User.new(user_params)
     if user.save
       login(user)
-      return render json: { status: 'ture', logged_in: true,id: user.id,  message: 'Signup success' }
+      render json: { status: 'true', logged_in: true, id: user.id, message: 'Signup success' }
     else
-      render json: { status: "error", errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json: { status: 'error', errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def show
     token = cookies[:token]
 
-    if token.present?
+    if token.blank?
       render json: { message: 'Unauthorized' }, status: :unauthorized
     else
       begin
         rsa_private = OpenSSL::PKey::RSA.new(File.read(Rails.root.join('auth/service.key')))
         decoded_token = JWT.decode(token, rsa_private, true, algorithm: 'RS256')
         user_id = decoded_token.first['sub']
-        user = User.find_by(id: user_id)
+        user = User.find_by(id: params[:id])
 
         if user
           render json: {
@@ -48,7 +49,7 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:user_id])
+    @user = User.find_by(id: params[:id])
 
     if @user
       render json: { status: 'success', data: { id: @user.id } }, status: :ok
@@ -65,7 +66,7 @@ class UsersController < ApplicationController
       render json: { status: 'error', message: 'User not found' }, status: :not_found
     end
   end
-  
+
   private
 
   def user_params
@@ -73,6 +74,6 @@ class UsersController < ApplicationController
   end
 
   def csrf_token
-    response.headers['X-CSRF-Token'] = form_authenticity_token
+    response.headers['X-CSRF-Token'] = request_forgery_protection_token if request_forgery_protection_token.present?
   end
 end
